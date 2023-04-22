0f5bacaf25388b0f1d8a5f9193a70d7cbae03eb7
Kellvyn Sampaio <64695440+TheSampaio@users.noreply.github.com>
2023-04-22 14:52:59 -0300

    Added commentaries to the shaders

--- Labs/Lab 05 - Shader Abstraction/DefaultFrag.glsl
+++ Labs/Lab 05 - Shader Abstraction/DefaultFrag.glsl
@@ -1,9 +1,14 @@
+// Sets the GLSL's version and compatibility mode
 #version 330 core
 
+// Input variables
 in vec4 outColour;
+
+// Output variables
 out vec4 gl_Color;
 
 void main()
 {
+    // Final fragment colour
     gl_Color = outColour;
 };
--- Labs/Lab 05 - Shader Abstraction/DefaultVert.glsl
+++ Labs/Lab 05 - Shader Abstraction/DefaultVert.glsl
@@ -1,12 +1,18 @@
+// Sets the GLSL's version and compatibility mode
 #version 330 core
 
+// Set-ups GLSL's layouts (Input variables)
 layout (location = 0) in vec3 inPosition;
 layout (location = 1) in vec3 inColour;
 
+// Output variables
 out vec4 outColour;
 
 void main()
 {
+    // Send data to the fragment shader
     outColour = vec4(inColour, 1.0);
+
+    // Final vertex position
     gl_Position = vec4(inPosition, 1.0);
 };
